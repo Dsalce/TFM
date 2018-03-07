82a66b1f505ee035760f5d7fe19a3b2c77130ff5
Dsalce <dsalce92@gmail.com>
2018-03-07 16:56:15 +0100

    Reglas 3

--- pru/pruReglas.py
+++ pru/pruReglas.py
@@ -1,4 +1,4 @@
- 
+
 """import sys
 import os
 import csv
@@ -31,15 +31,16 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-#df = pd.read_csv("/home/dsalce92/Documents/Fichero/PITLR5.csv")
-df = pd.read_excel("D:\David_inf\PITLR1.csv.xlsx")
+df = pd.read_csv("/home/dsalce92/Documents/Fichero/PITLR5.csv")
+#df = pd.read_excel("D:\David_inf\PITLR1.csv.xlsx")
 df = df.fillna(value="")
-#basket_sets = (df.groupby(["DEFEC.", "INSPECCION"]).sum().unstack().reset_index().fillna(0))#.set_index("INSPECCION"))
 #
-basket_sets = (df[df["GRUP"]==300].groupby(["DEFEC.", "INSPECCION"]).count().unstack(level=0).fillna(0))
+basket_sets = (df[df["GRUP"]==1000].groupby(["DEFEC.", "INSPECCION"]).count().unstack(level=0).fillna(0))
+
+
 #basket_sets=(df[df["GRUP"]==1000].pivot(rows="DEFEC.", columns="INSPECCION", aggfunc='count').fillna(0) )
-#print(basket_sets.head())
-basket_sets = basket_sets.iloc[80:]
+#print(basket_sets)
+basket_sets = basket_sets.iloc[5250:][650:]
 def encode_units(x):
     if x <= 0:
         return 0
@@ -50,15 +51,13 @@ basket_sets = basket_sets.applymap(encode_units)
 
 
 
-
-with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
-   print(basket_sets)
+print(basket_sets)
 
 
-frequent_itemsets = apriori(basket_sets, min_support=0.05, use_colnames=True)
-print(frequent_itemsets)
+frequent_itemsets = apriori(basket_sets,min_support=0.05, use_colnames=True)
+print(frequent_itemsets.head())
 rules = association_rules(frequent_itemsets, metric="lift", min_threshold="1")
-print(rules)
+print(rules.head())
 
 
 
