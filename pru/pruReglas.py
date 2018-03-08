b11e5ba2fc49da5b68026897db75bd004b68a916
Dsalce <dsalce92@gmail.com>
2018-03-08 22:34:42 +0100

    Reglas en proceso de integracion

--- pru/pruReglas.py
+++ pru/pruReglas.py
@@ -31,19 +31,14 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-df = pd.read_csv("/home/dsalce92/Documents/Fichero/PITLR5.csv") 
-#df = pd.read_excel("D:\David_inf\PITLR1.csv.xlsx")
+#df = pd.read_csv("/home/dsalce92/Documents/Fichero/PITLR5.csv") 
+df = pd.read_excel("D:\David_inf\PITLR1.csv.xlsx")
 df = df.fillna(value="")
 
 
 
-basket_sets = (df[df["GRUP"]==300].groupby(["DEFEC.", "INSPECCION"]).count().unstack(level=0).fillna(0))
 
-
-basket_sets = basket_sets.iloc[30:]
-
-
-
+basket_sets = (df[df['GRUP']==2011].groupby([ "INSPECCION","DEFEC."])["DEFEC."].count().unstack().fillna(0))
 
 def encode_units(x):
     if x <= 0:
@@ -52,28 +47,26 @@ def encode_units(x):
         return 1
 
 basket_sets = basket_sets.applymap(encode_units)
-
 print(basket_sets)
-
-
-
-
 frequent_itemsets = apriori(basket_sets,min_support=0.05, use_colnames=True)
-print(frequent_itemsets.head())
+print(frequent_itemsets)
 rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
-print(rules.head())
+print(rules)
 
 
 
-"""df = pd.read_excel("/home/dsalce92/Documents/Fichero/OnlineRetail.xlsx")
+#df = pd.read_excel("/home/dsalce92/Documents/Fichero/OnlineRetail.xlsx")
+"""df = pd.read_excel("D:\David_inf\OnlineRetail.xlsx")
 
 df['Description'] = df['Description'].str.strip()
 df.dropna(axis=0, subset=['InvoiceNo'], inplace=True)
 df['InvoiceNo'] = df['InvoiceNo'].astype('str')
 df = df[~df['InvoiceNo'].str.contains('C')]
 
-basket2 = (df.groupby(['InvoiceNo', 'Description'])['Quantity']
-          .sum().unstack().fillna(0))
+basket2 = (df[df['Country'] =="France"]
+          .groupby(['InvoiceNo', 'Description'])['Quantity']
+          .sum().unstack().reset_index().fillna(0)
+          .set_index('InvoiceNo'))
 
 def encode_units(x):
     if x <= 0:
@@ -82,10 +75,10 @@ def encode_units(x):
         return 1
 
 basket_sets2 = basket2.applymap(encode_units)
-#print(basket_sets2)
+print(basket_sets2)
 basket_sets2.drop('POSTAGE', inplace=True, axis=1)
 frequent_itemsets2 = apriori(basket_sets2, min_support=0.05, use_colnames=True)
-#print(frequent_itemsets2)
+print(frequent_itemsets2)
 rules2 = association_rules(frequent_itemsets2, metric="lift", min_threshold=1)
 
 print("Rules")
